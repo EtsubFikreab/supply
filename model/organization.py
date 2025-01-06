a640abddc84b16ca09e5351b2c57e4356c353ac8
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2025-01-06 22:39:03 +0300

    create org without name

--- model/organization.py
+++ model/organization.py
@@ -3,12 +3,13 @@ from sqlmodel import Field, SQLModel
 from typing import Optional
 import uuid
 
+
 class Organization(SQLModel, table=True):
     __tablename__ = "organization"
 
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: Optional[uuid.UUID] = Field(foreign_key="user_roles.user_id", default=None)  # Foreign key to auth.users
-    org_name: str = Field(max_length=255, nullable=False)
+    org_name: Optional[str] = Field(max_length=255, default=None)
     address: Optional[str] = None
     phone_number: Optional[str] = Field(default=None, max_length=15)
     email: Optional[str] = Field(default=None, max_length=255)
@@ -16,9 +17,12 @@ class Organization(SQLModel, table=True):
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
 
+
 class UserRoles(SQLModel, table=True):
     __tablename__ = "user_roles"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: uuid.UUID = Field(foreign_key="auth.users.id", nullable=False, unique=True)  # Unique foreign key to auth.users
+    # Unique foreign key to auth.users
+    user_id: uuid.UUID = Field(
+        foreign_key="auth.users.id", nullable=False, unique=True)
     role: str = Field(nullable=False)
